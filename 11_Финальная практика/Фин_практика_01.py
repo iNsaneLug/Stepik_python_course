c918343c440cc0712fb2fba26202efd7f54938d8
iNsaneLug <107928586+iNsaneLug@users.noreply.github.com>
2022-09-08 17:50:56 +0300

    11_Фин_практика

---
+++ 11_Финальная практика/Фин_практика_01.py
@@ -0,0 +1,29 @@
+# . Напишите функцию count_vowels, которая принимает строку и возвращает количество гласных в ней.
+#
+# Примеры вызовов и возвратов:
+#
+# count_vowels('abcd') -> 1
+# count_vowels('abcdef') -> 2
+# count_vowels('bcd') -> 0
+#
+# Примечание: речь идёт только о латинском алфавите.
+#
+# Sample Input:
+#
+# abc
+# Sample Output:
+#
+# 1
+
+
+
+def count_vowels(str1):
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for letter in str1:
+        if letter in vowel:
+            count += 1
+    return print(count)
+
+input_str = input(str())
+result = count_vowels(input_str)
